332b7fe7a1bdf539820fed88d873f2b9414f70c5
jayprophit <jprophit@gmail.com>
2023-12-14 15:34:59 +0000

    update

---
+++ calculator.py
@@ -0,0 +1,36 @@
+'''
+calculator
+'''
+
+
+x = input("what's x? ")
+y = input("what's y? ")
+
+# int converts it into a number to use in mathmatics
+z = int(x) + int(y)
+
+print(z)
+
+
+
+'''
+# different variations to produce the same result and solve the problem
+# asending from currently used practises to basic begginer code
+
+
+x = input("what's x? ")
+y = input("what's y? ")
+
+# concatenates, so this want give the correct answer
+z = x + y
+
+print(z)
+
+
+x = 1
+y = 2
+
+z = x + y
+
+print(z)
+'''
