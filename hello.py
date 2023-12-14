7e09e8b60bc2b139e4ee055fc76c69527d95e8a9
jayprophit <jprophit@gmail.com>
2023-12-14 08:57:34 +0000

    hello world - 1st programme

--- hello.py
+++ hello.py
@@ -1,1 +1,73 @@
+# print hello world to be interperated and made readable 
 print("hello world")
+
+
+'''
+Python is a program otherwise known as an interperator.  
+It converts the file into readable binary code a computer can understand,
+then it changes the programme into readable human language
+'''
+
+'''
+Using the terminal and typing 
+code name.py - 
+in the place of name, is where you put your file name
+this allows you to create a new file
+'''
+
+'''
+Using the terminal and typing
+python name.py
+in the place of name, is where you put your file name
+this will run your programme
+'''
+
+# 'number sign, hash, or pound sign' (#) is a comment, which allows you to leave notes that are reminders that are ignored by the programme
+
+'''
+ Three 'punctuation marks' (''' ''') before and after your text, 
+ allows comment of a section of information, 
+ to leave notes that are reminders that are ignored by the programme 
+'''
+
+'''
+Functions
+a function is like an action or a verb,
+that lets you do something in the programme.
+generally any language comes with some predetermined set of functions.
+the can be very basic actions or verbs, 
+that the language will already know how to use.
+as a programmer you can use the functions to get the computer to do those things
+'''
+
+'''
+Arguements
+is an input to a function that somehow influences its's behavior 
+'''
+
+'''
+print()
+Print is a function that use's parentheses, 
+as an ability to take in some input of string of text data, 
+in english or any other human language.
+that you want this function to print onto the screen
+'''
+
+'''
+Side Effects
+when a function is exicuterd. 
+The program gives back an action from the commands given.
+it can be:-
+
+visual
+audio
+'''
+
+'''
+Bugs
+are mistakes that coders and progammers make, 
+that causes errors in the programme
+and its the coders and progammers job to fix theses problems
+before, during and after programme launch.
+Theses are just seen as problems that need to be solved, edited, rewritten, modified or simplified
+'''
