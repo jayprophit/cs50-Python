ea2999d0d8de328393a6e81d8e1901da53ade7ea
jayprophit <jprophit@gmail.com>
2023-12-14 07:34:40 +0000

    first commit

---
+++ context_index.py
@@ -0,0 +1,15 @@
+'''
+functions
+varaibes
+consitionals - true or false
+loops - repeated again and again set number of times
+exceptions - except errors
+libraries - 3rd party code orcode you have writen to be reused as many times as desired
+unit test - write test to test your own code
+file i/o - input/ out
+regular expression - validate data or extract data
+objec-oriented programming:- allows you to represent real world entities
+- procedural programming - write procedures top to bottom to solve problems step by step
+- functinal programming - 
+et cetera
+'''
