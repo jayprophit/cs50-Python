268c9bafa2eb6d5685c4f525f08611f039503f34
jayprophit <jprophit@gmail.com>
2024-03-28 00:37:47 +0000

    Section 7

--- students.py
+++ students.py
@@ -36,6 +36,18 @@ students = []
 with open("students.csv") as file:
     reader = csv.DictReader(file)
     for row in reader:
+        students.append(row)
+
+for student in sorted (students, key=lambda student: student["name"]):
+    print(f"{student['name']} is from {student['home']}")
+
+
+
+students = []
+
+with open("students.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
         students.append({"name": row["name"], "home": row["home""]})
 
 for student in sorted (students, key=lambda student: student["name"]):
