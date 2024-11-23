1fee6808419ecf1422ac018564ae60ac2ac5bcd2
Jonathan Powell <jprophit@gmail.com>
2024-11-23 18:10:37 +0000

    update

--- Section 9 - Object-Oriented Programming/student.py
+++ Section 9 - Object-Oriented Programming/student.py
@@ -4,8 +4,6 @@ student
 
 class Student:
     def __init__(self, name, house):
-        if not name:
-            raise ValueError("Missing name")
         self.name = name
         self.house = house
 
@@ -13,15 +11,27 @@ class Student:
     def __str__(self):
         return f"{self.name} from {self.house}"
 
+
+    # Getter
+    @property
+    def name(self):
+        return self._name
     
+    # Setter
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+
+
     # Getter - gets some attributes
     @property
     def house(self):
         return self._house
 
-
     # Setter - sets some values
-    house.setter
+    @house.setter
     def house(self, house):
         if house not in ["Gryffindor", "Hufflepuff", "Ravensclaw", "Slytherin"]:
             raise ValueError("Invalid house")
