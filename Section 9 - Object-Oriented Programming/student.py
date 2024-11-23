86477dd8df0c3fe40ffeab867f4331ea17d68d67
Jonathan Powell <jprophit@gmail.com>
2024-11-23 14:41:16 +0000

    update

--- Section 9 - Object-Oriented Programming/student.py
+++ Section 9 - Object-Oriented Programming/student.py
@@ -10,9 +10,9 @@ def main():
     print(f"{student.name} from {student.house}")
 
 def get_student():
-    student = Student()
-    student.name = input("Name: ")
-    student.house = input("House: ")
+    name = input("Name: ")
+    house = input("House: ")
+    student = Student(name, house)
     return student
 
 if __name__ == "__main__":
