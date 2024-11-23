5f0aeb4cb5df1e63173a0d15e66d35d307ad9679
Jonathan Powell <jprophit@gmail.com>
2024-11-23 11:01:57 +0000

    update

--- Section 8/twitter.py
+++ Section 8/twitter.py
@@ -5,7 +5,7 @@ import re
 
 url = input("URL: ").strip()
 
-re.sub(r"https://twitter.com", "", url)
+re.sub(r"^(https?://)?(www\.)?twitter\.com/", "", url)
 print(f"Username: {username}")
 
 
@@ -13,6 +13,18 @@ print(f"Username: {username}")
 '''
 # different variations to produce the same result and solve the problem
 # descending from currently used practices to basic beginner code
+
+
+re.sub(r"^http|https://twitter\.com/", "", url)
+print(f"Username: {username}")
+
+
+
+
+re.sub(r"^https://twitter\.com/", "", url)
+print(f"Username: {username}")
+
+
 
 
 username = url.removeprfix("https://twitter.com/", "")
