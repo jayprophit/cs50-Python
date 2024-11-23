81ce5297f86fba372c767aadbcb5376c7ad78d6c
Jonathan Powell <jprophit@gmail.com>
2024-11-23 11:46:59 +0000

    update

---
+++ Section 9 - Object-Oriented Programming/student.py
@@ -0,0 +1,20 @@
+'''
+student
+'''
+
+
+name = input("Name: ")
+house = input("House: ")
+print(f"{name} from {house}")
+
+
+'''
+# different variations to produce the same result and solve the problem
+# descending from currently used practices to basic beginner code
+
+
+
+name = input("Name: ")
+house = input("House: ")
+print(f"{name} from {house}")
+'''
