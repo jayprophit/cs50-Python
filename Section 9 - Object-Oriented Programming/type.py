c3513b8a2ed1d5cda932c84b704de3e04177e0fd
Jonathan Powell <jprophit@gmail.com>
2024-11-23 18:29:50 +0000

    update

--- Section 9 - Object-Oriented Programming/type.py
+++ Section 9 - Object-Oriented Programming/type.py
@@ -1,1 +1,35 @@
+'''
+type
+'''
+
+print(type(dict()))
+
+
+'''
+# different variations to produce the same result and solve the problem
+# descending from currently used practices to basic beginner code
+
+
+
+print(type(dict()))
+
+
+
+print(type({}))
+
+
+
+print(type(list()))
+
+
+
+print(type([]))
+
+
+
+print(type("hello, world"))
+
+
+
 print(type(50))
+'''
