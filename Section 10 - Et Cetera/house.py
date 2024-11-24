dc013520d4e9375914b5be0a1d73c9e1ac05e995
Jonathan Powell <jprophit@gmail.com>
2024-11-24 09:14:09 +0000

    update

---
+++ Section 10 - Et Cetera/house.py
@@ -0,0 +1,20 @@
+'''
+house
+'''
+
+student = [
+    {"name": "Hermione", "house", "Gryffindor"},
+    {"name": "Harry", "house", "Gryffindor"},
+    {"name": "Ron", "house", "Gryffindor"},
+    {"name": "Draco", "house", "Sltherin"},
+    {"name": "Padma", "house", "Ravenclaw"},
+]
+
+
+'''
+# different variations to produce the same result and solve the problem
+# descending from currently used practices to basic beginner code
+
+
+
+'''
