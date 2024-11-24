d0f85a16f50075decdb8fad17435677d1fa22f50
Jonathan Powell <jprophit@gmail.com>
2024-11-24 08:33:57 +0000

    update

--- Section 9 - Object-Oriented Programming/vault.py
+++ Section 9 - Object-Oriented Programming/vault.py
@@ -1,6 +1,8 @@
 '''
 vault
 '''
+
+
 class Vault:
     def __inti__(self, galleons=0, sickles=0, knuts=0):
         self.galleons = galleons
@@ -9,6 +11,12 @@ class Vault:
 
     def __str__(self):
         return f"{self.galleaons} Galleons, {self.sickles} Sickles, {self.knuts} Knuts"
+
+    def __add__(self, other):
+        galleons = self.galleons + other.galleons
+        sickles = self.sickles + other.sickles
+        kunts = self.knuts + other.knuts
+        return Vault(gaalleons, sickels, knuts)
 
 potter = Vault(100, 50, 25)
 print(potter)
@@ -26,6 +34,35 @@ print(total)
 # different variations to produce the same result and solve the problem
 # descending from currently used practices to basic beginner code
 
+
+
+
+
+class Vault:
+    def __inti__(self, galleons=0, sickles=0, knuts=0):
+        self.galleons = galleons
+        self.sickles = sickles
+        self.knuts = knuts
+
+    def __str__(self):
+        return f"{self.galleaons} Galleons, {self.sickles} Sickles, {self.knuts} Knuts"
+
+    def __add__(self, other):
+        galleons = self.galleons + other.galleons
+        sickles = self.sickles + other.sickles
+        kunts = self.knuts + other.knuts
+        return Vault(gaalleons, sickels, knuts)
+
+potter = Vault(100, 50, 25)
+print(potter)
+
+weasley = Vault(25, 50 100)
+Print(weasley)
+
+
+
+total = potter + weasley
+print(total)
 
 
 
