be4e700c157039b097d4da9add90a92e9e74d67d
Jonathan Powell <jprophit@gmail.com>
2024-11-24 09:30:25 +0000

    update

---
+++ Section 10 - Et Cetera/bank.py
@@ -0,0 +1,64 @@
+'''
+bank
+'''
+
+balance = 0
+
+
+def main():
+    print("Balance", balance)
+    deposite(100)
+    withdraw(50)
+    print("Balance", balance)
+
+def deposite(n):
+    balance += n
+
+def withdraw(n):
+    balance -= n
+
+if __name__ == "__main__"
+    main()
+
+
+
+'''
+# different variations to produce the same result and solve the problem
+# descending from currently used practices to basic beginner code
+
+
+
+
+
+balance = 0
+
+
+def main():
+    print("Balance", balance)
+    deposite(100)
+    withdraw(50)
+    print("Balance", balance)
+
+def deposite(n):
+    balance += n
+
+def withdraw(n):
+    balance -= n
+
+if __name__ == "__main__"
+    main()
+
+
+
+
+balance = 0
+
+
+def main():
+    print("Balance", balance)
+
+
+if __name__ == "__main__"
+    main()
+
+'''
