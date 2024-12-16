a1dc282d1a46078db125256c2a6b87f76ed3275c
Jonathan Powell <jprophit@gmail.com>
2024-12-16 09:46:27 +0000

    update

--- Python Documentation.md
+++ Python Documentation.md
@@ -1783,3 +1783,15 @@ def total(galleons, sickles, knuts):
 coins = [100, 50, 25]
 
 print(total(*coins), "knuts")
+
+## **input
+allows you to unpack a dictionary with the keys and the values
+
+### example:-
+
+def total(galleons, sickles, knuts):
+    return(galleons * 17 +sickles) * 29 + knuts
+
+coins = {"galleons": 100, "sickles": 50, "knuts": 25}
+
+print(total(**coins), "knuts")
--- Section 10 - Et Cetera/unpack.py
+++ Section 10 - Et Cetera/unpack.py
@@ -5,14 +5,35 @@ unpack
 def total(galleons, sickles, knuts):
     return(galleons * 17 +sickles) * 29 + knuts
 
-coins = [100, 50, 25]
+coins = {"galleons": 100, "sickles": 50, "knuts": 25}
 
-print(total(*coins), "knuts")
+print(total(**coins), "knuts")
 
 
 '''
 # different variations to produce the same result and solve the problem
 # descending from currently used practices to basic beginner code
+
+
+
+
+
+def total(galleons, sickles, knuts):
+    return(galleons * 17 +sickles) * 29 + knuts
+
+coins = {"galleons": 100, "sickles": 50, "knuts": 25}
+
+print(total(coins[galleons], coins[sickles], coins[knuts]), "knuts")
+
+
+
+
+def total(galleons, sickles, knuts):
+    return(galleons * 17 +sickles) * 29 + knuts
+
+print(total(galleons=100, sickles=50, knuts=25), "knuts")
+
+
 
 
 def total(galleons, sickles, knuts):
