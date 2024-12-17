eb567a3a4fc7985cd9c6831729b19c0925abf9f2
Jonathan Powell <jprophit@gmail.com>
2024-12-17 13:15:38 +0000

    update

---
+++ Section 10 - Et Cetera/gryffindors.py
@@ -0,0 +1,27 @@
+'''
+gryffindors
+'''
+
+students = [
+    {"name": "Harmione", "house": "Gryffindor"}
+    {"name": "Harry", "house": "Gryffindor"}
+    {"name": "Ron", "house": "Gryffindor"}
+    {"name": "Draco", "house": "Slytherin"}
+]
+
+
+'''
+# different variations to produce the same result and solve the problem
+# descending from currently used practices to basic beginner code
+
+
+
+
+
+students = [
+    {"name": "Harmione", "house": "Gryffindor"}
+    {"name": "Harry", "house": "Gryffindor"}
+    {"name": "Ron", "house": "Gryffindor"}
+    {"name": "Draco", "house": "Slytherin"}
+]
+'''
