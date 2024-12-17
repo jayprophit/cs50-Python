99893c5989fa5b4fb566760da5d18ffcfa023a92
Jonathan Powell <jprophit@gmail.com>
2024-12-17 13:43:28 +0000

    update

--- Section 10 - Et Cetera/gryffindors.py
+++ Section 10 - Et Cetera/gryffindors.py
@@ -9,17 +9,45 @@ students = [
     {"name": "Draco", "house": "Slytherin"}
 ]
 
-gryffindor = [
-    student["name"] for student in students if student["house"] == "Gryffindor"
-]
+def is_gryffindor(s):
+    return s["house"] == "Gryffindor":
 
-for gryffindor in sorted(gryffindors):
-    print(gryffindor)
-
+gryffindors = filter(is_gryffindor, students)
 
 '''
 # different variations to produce the same result and solve the problem
 # descending from currently used practices to basic beginner code
+
+
+
+
+
+students = [
+    {"name": "Harmione", "house": "Gryffindor"}
+    {"name": "Harry", "house": "Gryffindor"}
+    {"name": "Ron", "house": "Gryffindor"}
+    {"name": "Draco", "house": "Slytherin"}
+]
+
+def is_gryffindor(s):
+    return s["house"] == "Gryffindor":
+
+
+
+
+students = [
+    {"name": "Harmione", "house": "Gryffindor"}
+    {"name": "Harry", "house": "Gryffindor"}
+    {"name": "Ron", "house": "Gryffindor"}
+    {"name": "Draco", "house": "Slytherin"}
+]
+
+def is_gryffindor(s):
+    if s["house"] == "Gryffindor":
+        return True
+    else:
+        return False
+
 
 
 students = [
@@ -35,7 +63,6 @@ gryffindor = [
 
 for gryffindor in sorted(gryffindors):
     print(gryffindor)
-
 
 
 
@@ -50,7 +77,6 @@ students = [
 gryffindor = [
     student["name"] for student in students if student["house"] == "Gryffindor"
 ]
-
 
 
 
